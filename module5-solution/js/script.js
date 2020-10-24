ac722b683dd110ea549a48ded01410ee7bf25362
Claudia Arellano <cloud.arellano@gmail.com>
2020-10-24 15:20:12 -0500

    small change in .js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       //
        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,
                    "randomCategoryShortName",
-                   '{{chosenCategoryShortName}}');
+                   "{{'chosenCategoryShortName'}}");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
